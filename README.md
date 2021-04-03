06b37ab921d272ad0ef6dc8eaa1f63fb903957a1
Nick LaMonica <nila2238@lasp.colorado.edu>
2021-04-03 17:15:54 -0600

    add yaml file and requirements file

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Stock Sentiment Analyis
 
-### Bryce Woods and Nicholas LaMonica
+#### Bryce Woods and Nicholas LaMonica
 
-We are making a stock sentiment analyis program that attempts
-to predict the movements of stocks based on the prevailing sentiment from social media websites. 
+We are making a stock sentiment analysis program that attempts
+to predict the movements of stocks based on the prevailing sentiment from social media websites.
+
+### How To run
+
+1. pip install -r requirements.txt
+2. change `sample_config.yaml` to `config.yaml` and fill in your api keys and other configurations
+3. python main.py or run in pycharm
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import yaml
+import requests
+import pandas as pd
+import json
+import ast
+import yaml
 
 
-# Press the green button in the gutter to run the script.
+def config_loader(filepath):
+    with open(filepath, 'r') as file:
+        config = yaml.load(file, Loader=yaml.BaseLoader)
+    return config
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print('Hello world')
+    print(config_loader("config.yaml"))
