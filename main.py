c7b7816eaf89ee9b58f519a725c09188ed428dc3
thebmw37 <brycewoods37@gmail.com>
2021-04-09 16:19:12 -0600

    Reddit API working

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import yaml
 import requests
-import pandas as pd
+#import pandas as pd
 import json
 import ast
-import yaml
+from collections import Counter
 
 
 def config_loader(filepath):
@@ -17,5 +17,39 @@ def config_loader(filepath):
 
 
 if __name__ == '__main__':
-    print('Hello world')
-    print(config_loader("config.yaml"))
+    auth = requests.auth.HTTPBasicAuth("xQVhO_pVZaf_JA", "ZqXWXfLGyMy4uOKewtSOYiyUygG6iQ")
+
+    # Data for auth
+    data = {
+        'grant_type' : 'password',
+        'username' : 'thebmw37x',
+        'password' : 'bigyeet1'
+    }
+
+    # Version of this application
+    headers = {'User-Agent' : 'test/0.0.1'}
+
+    # Getting access token
+    res = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=headers)
+    TOKEN = res.json()['access_token']
+    headers['Authorization'] = f'bearer {TOKEN}'
+
+    # Getting hot posts from r/wallstreetbets
+    wsb_hot = requests.get('https://oauth.reddit.com/r/wallstreetbets/hot', headers=headers).json()
+
+    # Creating list of post titles
+    wsb_titles = ""
+
+    for post in wsb_hot['data']['children']:
+        wsb_titles += post['data']['title']
+
+    split_wsb_titles = wsb_titles.split()
+
+    # Counting instances of most popular words
+    Counter = Counter(split_wsb_titles)
+
+    # 25 most common words in the titles
+    wsb_common = Counter.most_common(25)
+
+    for word in wsb_common:
+        print(word)
