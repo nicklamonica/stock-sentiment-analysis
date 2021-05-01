5a64584503020359c6ccd5a4b65ab4f2b2766a75
Nicholas LaMonica <nila2238@colorado.edu>
2021-05-01 09:53:52 -0600

    added gru class, moved saved model to models directory

---
+++ models/gru.py
@@ -0,0 +1,19 @@
+from models.rnn import Rnn
+
+from tensorflow.keras.layers import Dense, Dropout, GRU, Embedding
+
+
+class Gru(Rnn):
+
+    def __init__(self):
+        super().__init__()
+
+    def build_model(self, input_length):
+        # input_length = X.shape[1]
+        self.model.add(Embedding(5000, 256, input_length=input_length))
+        self.model.add(Dropout(0.3))
+        self.model.add(GRU(256, return_sequences=True, dropout=0.3, recurrent_dropout=0.2))
+        self.model.add(GRU(256, dropout=0.3, recurrent_dropout=0.2))
+        self.model.add(Dense(2, activation='softmax'))
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        self.model.summary()
